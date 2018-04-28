52d450d362b25ff0cc116a7e3b0778304f3f4488
MrsLeffew <tanya.leffew@gmail.com>
2018-04-27 19:09:12 -0600

    finished

--- twitch.js
+++ twitch.js
@@ -1,10 +1,11 @@
-const channels = ['riotgames', 'freecodecamp', 'puppers', 'nugiyen', 'tru3ta1ent'];
-let data = [];
+const channels = ['riotgames', 'freecodecamp', 'puppers', 'nugiyen', 'tru3ta1ent', 'mathil1'];
+let data = []; // [name: '', logo: '', status: '', url: '', game: '']
 
 const url = (type, channel) => (`https://wind-bow.gomix.me/twitch-api/${type}/${channel}?callback=?`);
 
 const fetchChannelData = (channel) => (
   $.getJSON(url('channels', channel), response => {
+    //console.log('channel', response);
     data.push({
       name: response.name,
       logo: response.logo,
@@ -16,19 +17,23 @@ const fetchChannelData = (channel) => (
       stream: '',
       status: '',
     })
-  })
-);
 
+  })
+); // ..rest
 const fetchStreamData = (channel) => (
   $.getJSON(url('streams', channel), response => {
     console.log('streams', response);
     //filter through stream for live responses
     const objIndex = data.findIndex(obj => obj.name.toLowerCase() === channel.toLowerCase());
 
+    // console.log(2, response.stream.channel.status);
+
     response.stream === null ? data[objIndex].stream = "Offline" : data[objIndex].stream = "Online";
 
-    if (data.stream === "Online") {
-      data.status = response.stream.channel.status;
+    if (data[objIndex].stream === "Online") {
+      console.log(5, data[objIndex].status);
+      data[objIndex].status = response.stream.channel.status;
+      console.log(6, data[objIndex].status);
     }
 
     if(channel === channels[channels.length - 1]){
@@ -46,17 +51,17 @@ const fetchStreamData = (channel) => (
 
 
 $('#all').on('click', () => {
+  $('#content').empty();
   (displayData(data));
-  showColor();
 });
 $('#online').on('click', () => {
+  $('#content').empty();
   (displayData(data.filter(online => online.stream == "Online")));
-  showColor();
 });
 
 $('#offline').on('click', () => {
+  $('#content').empty();
   (displayData(data.filter(offline => offline.stream == "Offline")));
-  showColor();
 });
 
 // const handleSearch = (value) => ();
@@ -64,15 +69,24 @@ $('#offline').on('click', () => {
 const displayData = (data) => {
   console.log('data', data)
   $('#content').empty();
-  data.forEach(item => $('#content').append(`<a href="${item.url}" target="_blank"> <div class="streamers"><img class="icon" src=${item.logo}> ${item.display} ${item.game} ${item.stream}</div>`));
+
+  data.forEach(item => $('#content').append(`<a href="https://www.twitch.tv/${item.name}" target="_blank">
+<div class="streamers row">
+  <div class="col-sm-2"><img class="icon" src=${item.logo}></div>
+  <div class="col-sm-4">${item.display}</div>
+  <div class="col-sm-5">${item.status ? item.status : item.stream}</div>
+</div>`));
 };
 
-const showColor = () => {
-  $('#all').toggleClass('color');
-  $('#online').toggleClass('color');
-  $('#offline').toggleClass('color');
-
-};
+const header = document.getElementById("btnDIV");
+const btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
 ( () => {
   fetchData();
